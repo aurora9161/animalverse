6baf365261f533961969dad0c72142cc418d7dbb
aurora9161 <proballs56@gmail.com>
2025-12-09 18:33:52 +0530

    Update utils to include APIHandler

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 """AnimalVerse utilities package"""
 from .database import JSONDatabase, GuildSettings, UserStats
+from .api_handler import APIHandler
 
-__all__ = ['JSONDatabase', 'GuildSettings', 'UserStats']
+__all__ = ['JSONDatabase', 'GuildSettings', 'UserStats', 'APIHandler']
